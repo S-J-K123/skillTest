1e08cbc4fc64fa8f0cfd63b8983dc50066abcd8b
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2023-09-15 08:36:37 +0100

    cs

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,70 +1,66 @@
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import React from 'react';
+import React from "react";
 import { useRef } from "react";
-import picOne from '../assets/pic-1.png';
-import picTwo from '../assets/pic-2.png';
-import picThree from '../assets/pic-3.png';
-
-
+import picOne from "../assets/pic-1.png";
+import picTwo from "../assets/pic-2.png";
+import picThree from "../assets/pic-3.png";
 
 function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", background: "red" }}
-        onClick={onClick}
-      />
-    );
-  }
-  
-  function SamplePrevArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", background: "green" }}
-        onClick={onClick}
-      />
-    );
-  }
-  
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+      onClick={onClick}
+    />
+  );
+}
 
-  
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
 
 const Carousel = () => {
-    const settings = {
-        infinite: false,
-        speed: 700,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />
-    
-    }
+  const settings = {
+    infinite: false,
+    speed: 700,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
 
-    const slider = useRef(null);
-    return (
-     
-  <div>
-       
-        <Slider {...settings} ref={slider}>
-          <div>
-            <h3><img className="picOne" src={picOne} alt="pic one" /></h3>
-          </div>
-          <div>
-            <h3><img className="picTwo" src={picTwo} alt="pic two" /></h3>
-          </div>
-          <div>
-            <h3><img className="picThree" src={picThree} alt="pic-three" /></h3>
-          </div>
-     
-        </Slider>
-      </div>
-    
-    );
-}
+  const slider = useRef(null);
+  return (
+    <div className="container">
+      <Slider {...settings} ref={slider}>
+        <div>
+          <h3>
+            <img className="picOne" src={picOne} alt="pic one" />
+          </h3>
+        </div>
+        <div>
+          <h3>
+            <img className="picTwo" src={picTwo} alt="pic two" />
+          </h3>
+        </div>
+        <div>
+          <h3>
+            <img className="picThree" src={picThree} alt="pic-three" />
+          </h3>
+        </div>
+      </Slider>
+    </div>
+  );
+};
 
 export default Carousel;
